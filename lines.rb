999c9bfeab47270b92a54ea4b3add7e8a5c0f2c0
Carlos <carlos_akemolo@hotmail.com>
2012-09-10 13:18:01 -0700

    line.rd script method finished

--- lines.rb
+++ lines.rb
@@ -1,1 +1,9 @@
-#Ruby script that that reports the non-blank, non-commented lines in the file named by the first argument.
+#Ruby script that that reports the non-blank, non-commented lines in the file named by the first argument
+
+total= 0
+File.open(ARGV[0]).each do|line|
+	unless  /(^#)|(^\s*$)/ =~ line 
+		total +=1	
+	end 
+end
+puts total
